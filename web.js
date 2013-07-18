e9408aa16981e24daece5050c2a88531b7881d3a
Dahyr Vergara <dahyrjose@gmail.com>
2013-07-18 14:24:59 +0000

    Reading index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  var r = fs.readFileSync('./index.html', 'utf-8');
+  response.send(r);
 });
 
 var port = process.env.PORT || 5000;
